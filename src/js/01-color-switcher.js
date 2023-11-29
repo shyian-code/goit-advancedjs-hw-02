2b88a15e333e82ce0c0876c7e9e4b8085fc3f826
Oleksandr Shyian <shyian.code@gmail.com>
2023-11-29 02:11:10 +0200

    done color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
-
+// 01-color-switcher.js
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+  
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+  
+let intervalId = null;
+  
+  const startColorSwitching = () => {
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  
+    startButton.disabled = true;
+  };
+  
+  const stopColorSwitching = () => {
+    clearInterval(intervalId);
+    startButton.disabled = false;
+  };
+  
+startButton.addEventListener('click', startColorSwitching);
+stopButton.addEventListener('click', stopColorSwitching);
+  
