e5448d55470d32cb9559a5ed05c1e34a07097426
Oleksandr Shyian <shyian.code@gmail.com>
2023-12-01 15:57:37 +0200

    changed code for timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,89 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const datetimePicker = document.getElementById("datetime-picker");
+const startButton = document.querySelector("[data-start]");
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate <= currentDate) {
+      // Показати повідомлення про помилку
+      iziToast.error({
+        title: "Error",
+        message: "Please choose a date in the future",
+      });
+    } else {
+      // Активувати кнопку "Start"
+      startButton.removeAttribute("disabled");
+    }
+  },
+};
+
+
+flatpickr(datetimePicker, options);
+
+
+startButton.addEventListener("click", startCountdown);
+
+function startCountdown() {
+  const selectedDate = datetimePicker._flatpickr.selectedDates[0];
+
+  const timeRemaining = selectedDate - new Date();
+
+  countdown(timeRemaining);
+}
+
+function countdown(ms) {
+  const timerFields = {
+    days: document.querySelector("[data-days]"),
+    hours: document.querySelector("[data-hours]"),
+    minutes: document.querySelector("[data-minutes]"),
+    seconds: document.querySelector("[data-seconds]"),
+  };
+
+  function updateTimer() {
+    const { days, hours, minutes, seconds } = convertMs(ms);
+
+    timerFields.days.textContent = addLeadingZero(days);
+    timerFields.hours.textContent = addLeadingZero(hours);
+    timerFields.minutes.textContent = addLeadingZero(minutes);
+    timerFields.seconds.textContent = addLeadingZero(seconds);
+
+    if (ms <= 0) {
+      clearInterval(interval);
+    } else {
+      ms -= 1000;
+    }
+  }
+
+  const interval = setInterval(updateTimer, 1000);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, "0");
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+
+
+document.querySelector(".form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  const delay = parseInt(delayInput.value, 10);
+  const step = parseInt(stepInput.value, 10);
+  const amount = parseInt(amountInput.value, 10);
+
+  if (isNaN(delay) || isNaN(step) || isNaN(amount)) {
+    iziToast.error({
+      title: "Error",
+      message: "Please enter valid numeric values.",
+    });
+    return;
+  }
+
+  createPromises({ delay, step, amount });
+});
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function createPromises({ delay, step, amount }) {
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay + (i - 1) * step)
+      .then(({ position, delay }) => {
+        iziToast.success({
+          title: "Fulfilled",
+          message: `Promise ${position} fulfilled in ${delay}ms`,
+        });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.error({
+          title: "Rejected",
+          message: `Promise ${position} rejected in ${delay}ms`,
+        });
+      });
   }
 }
