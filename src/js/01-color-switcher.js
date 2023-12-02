645f31067c3b74733b24d9b0053c3b8d0a5c6c70
Oleksandr Shyian <shyian.code@gmail.com>
2023-12-02 17:43:05 +0200

    fix button disabling functionality for color swithcer

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,26 +1,29 @@
-// 01-color-switcher.js
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
-  
+
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
-  
+
 let intervalId = null;
-  
-  const startColorSwitching = () => {
-    intervalId = setInterval(() => {
-      document.body.style.backgroundColor = getRandomHexColor();
-    }, 1000);
-  
-    startButton.disabled = true;
-  };
-  
-  const stopColorSwitching = () => {
-    clearInterval(intervalId);
-    startButton.disabled = false;
-  };
-  
+
+const startColorSwitching = () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startButton.disabled = true;
+  stopButton.disabled = false;
+};
+
+const stopColorSwitching = () => {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+};
+
+// Додавання деактивації кнопки Stop при завантаженні сторінки
+stopButton.disabled = true;
+
 startButton.addEventListener('click', startColorSwitching);
 stopButton.addEventListener('click', stopColorSwitching);
-  
